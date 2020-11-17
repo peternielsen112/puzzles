91ac57895208dcb14a6b1fa84e8e8b6b5f3b9f1a
Peter Nielsen <dirigo112@gmail.com>
2020-11-17 12:12:26 -0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,15 +4,6 @@
 # in this puzzle, you'll need to make a loop that prints every number in a user's input.
 
 
-def debug():
-  try:
-    import time, os, sys, random
-    # you might not need all these modules for this puzzle, but you'll have to figure it out!
-  except ImportError:
-    print('Your modules didn\'t import correctly... try fixing your spelling!')
-debug()
-
-
 # start with setting a variable, x, to zero:
 x = 0
 userinput = input('prompt')
